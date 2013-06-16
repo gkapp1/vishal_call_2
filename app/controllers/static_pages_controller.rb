fb7f69b3539603c19a508143c28dc8ef4160c6c9
Greg Kapp <gregkapp@gmail.com>
2013-06-16 15:19:02 -0700

    changed number given vishal quality of life complaint

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,7 +7,7 @@ class StaticPagesController < ApplicationController
 
   private
   def send_call
-    receiver = '+17323257565'
+    receiver = '+14152254973'
     phone_number = '+16502739905'
     account_sid = 'ACde16f587e2e3db8b5e00d2f1d1f15fb3'
     auth_token = 'f758b3ad8c03bbdae2612ebf1a936d1f'
